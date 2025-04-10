8757a3f2e7893053790b4881fb9e3352bbebbc36
thomas7917 <r.monaccuia67@gmail.com>
2025-04-10 13:31:14 +0200

    primo commit

--- app/src/main/java/com/example/rollthedice_monacchia/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice_monacchia/MainActivity.kt
@@ -1,6 +1,11 @@
-package com.example.rollthedice_monacchia
+package com.example.rollthedice5bi
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +17,12 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        var 
+        var btnRoll = findViewById<Button>(R.id.buttonLancia)
+        var txtTitle = findViewById<TextView>(R.id.textViewtitle)
+        var imgView = findViewById<ImageView>(R.id.imageView)
+        btnRoll.setOnClickListener(View.OnClickListener {
+            var mioToast = Toast.makeText(this,"DADO LANCIATO!!", Toast.LENGTH_LONG)
+            mioToast.show()
+        })
     }
 }
